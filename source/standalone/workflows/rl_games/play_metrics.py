baa5732a1d398d465cb50272023ac38e5a0e940f
Fousseyni-Sang <fsangaref@gmail.com>
2025-07-10 15:18:54 +0200

    nothing

--- source/standalone/workflows/rl_games/play_metrics.py
+++ source/standalone/workflows/rl_games/play_metrics.py
@@ -204,7 +204,7 @@ def main():
     #try:
     #while simulation_app.is_running():
     current_step = 0
-    wind_direc = (125*torch.pi/180)
+    wind_direc = (150*torch.pi/180)
     wind_speed = 5
     while episode_cntr<num_episodes:   
         
